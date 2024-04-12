b2db0d302850ac9051a7ed3a087e660988fa1ee5
Whilmar Bitoco <whlmrbitoco@gmail.com>
2024-04-12 13:28:20 +0800

    remove duplicate

--- src/RemoveDuplicate.java
+++ src/RemoveDuplicate.java
@@ -4,20 +4,29 @@ public class RemoveDuplicate {
 	public static void main(String[] args) {
 		int[] arr = {3,6,26,65,22,25,56,13,67,21,67,3,5,6,26};
 		
-		printArr(arr);
-		removeDuplicate(arr);
+//		sort(arr);
+		int[] tmp = sort(arr);
 		
-		printArr(arr);
+		int[] tmp2 = removeDuplicate(tmp);
+		printArr(tmp2);
 	}
 
 	private static int[] removeDuplicate(int[] arr) {
 		int unique[] = new int[arr.length];
 		
 		for (int i = 0; i < arr.length - 1; i++) {
-			if (arr[i] != arr[i + 1]) {
-				unique[i] = arr[i];
+			int current = arr[i];
+			
+			for (int j = 0; j < arr.length - i - 1; j++) {
+				
+				if (arr[j] != arr[j + 1]) {
+					unique[j] = arr[j];
+				
+				}
 			}
-		}
+		} 
+		
+	
 		
 		return unique;
 		
@@ -29,5 +38,21 @@ public class RemoveDuplicate {
 			System.out.print(a + " ");
 		}
 	System.out.println();
+	}
+	
+	public static int[] sort(int[] arr) {
+		
+		int tmp;
+		
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = 0; j < arr.length - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					tmp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = tmp;
+				}
+			}
+		}
+		return arr;
 	}
 }
